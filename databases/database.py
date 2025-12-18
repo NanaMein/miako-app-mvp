87c0ae25ad73cef47249aaee1f5a65b27f539ebd
Nana Mein <alfeoephraimquintay@gmail.com>
2025-12-18 13:11:28 +0800

    Database async session and postgres integration

---
+++ databases/database.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL=os.getenv("DATABASE_URL")
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    future=True
+)
+
+async_session_maker = sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
