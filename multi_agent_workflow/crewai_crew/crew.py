f04cb011d048b2d31c78aad489e4efb161ed0050
Nana Mein <alfeoephraimquintay@gmail.com>
2025-12-19 22:59:54 +0800

    LLM Working and fixed the pathlib because there is an error when i use the one in crewai example.

--- multi_agent_workflow/crewai_crew/crew.py
+++ multi_agent_workflow/crewai_crew/crew.py
@@ -1,12 +1,30 @@
+import os
+from pathlib import Path
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
+from multi_agent_workflow.llms.custom_llm_for_crewai import GroqLLM
 from typing import List
 
+
+def groq_llm():
+    return GroqLLM(
+        model=llm_sample_selector(3),
+        api_key=os.getenv("GROQ_API_KEY"),
+        max_tokens=8000,
+    )
 
 @CrewBase
 class MultiAgentWorkflow:
     """MultiAgentWorkflow crew"""
+
+    base_path = Path(__file__).parent.parent
+
+    agents_config = str(base_path / "config" / "agents.yaml")
+    tasks_config = str(base_path / "config" / "tasks.yaml")
+
+    def __init__(self):
+        self.llm = groq_llm()
 
     agents: List[BaseAgent]
     tasks: List[Task]
@@ -15,6 +33,7 @@ class MultiAgentWorkflow:
     def researcher(self) -> Agent:
         return Agent(
             config=self.agents_config['researcher'], # type: ignore[index]
+            llm=self.llm,
             verbose=True
         )
 
@@ -22,6 +41,7 @@ class MultiAgentWorkflow:
     def reporting_analyst(self) -> Agent:
         return Agent(
             config=self.agents_config['reporting_analyst'], # type: ignore[index]
+            llm=self.llm,
             verbose=True
         )
 
@@ -41,8 +61,8 @@ class MultiAgentWorkflow:
     @crew
     def crew(self) -> Crew:
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents,
+            tasks=self.tasks,
             process=Process.sequential,
             verbose=True,
         )
