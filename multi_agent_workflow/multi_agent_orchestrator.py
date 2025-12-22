9fff2479724b35642c6922ca6099df1011e91a1e
Nana Mein <alfeoephraimquintay@gmail.com>
2025-12-22 23:11:56 +0800

    Changed the input from str to dict with different input format like json, list and dict. ALthough i have problem with datetime since it appears to have error. so i convert it to str

--- multi_agent_workflow/multi_agent_orchestrator.py
+++ multi_agent_workflow/multi_agent_orchestrator.py
@@ -12,17 +12,16 @@ from multi_agent_workflow.crewai_crew.crew import MultiAgentWorkflow as AgentsWo
 
 agents = AgentsWorkflow()
 
-def workflow_orchestrator(inputs: str):
+def workflow_orchestrator(inputs: Any):
     try:
-        inputs = {
-            "topic": inputs
-        }
         crewai = agents.crew().kickoff(inputs=inputs)
         return crewai.raw
     except Exception as ex:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Unexpected Error Handling: {ex}")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Unexpected Error Handling: {ex}"
+        )
 
-from models.message_model import MessageBase, Role
 
 
 app = FastAPI()
