736cc4f6dc6d241692c7e7965b57d666d71db139
Nana Mein <alfeoephraimquintay@gmail.com>
2025-12-23 13:30:02 +0800

    Added new version, it only sends one conversation list and uses run in threadpool for better performance and dont blocking other request in an async thread.

--- multi_agent_workflow/multi_agent_orchestrator.py
+++ multi_agent_workflow/multi_agent_orchestrator.py
@@ -8,6 +8,7 @@ from fastapi import Depends
 from sqlmodel import select, SQLModel, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from multi_agent_workflow.crewai_crew.crew import MultiAgentWorkflow as AgentsWorkflow
+from datetime import datetime, timezone
 
 
 
@@ -74,6 +75,41 @@ async def send_async_msg_v1(payload: MessageBase, session: AsyncSession = Depend
     await session.refresh(asst_msg)
     return asst_msg
 
+
+@app.post("/v2/send-message", response_model=MessageBase, status_code=status.HTTP_201_CREATED)
+async def send_async_msg_v1(payload: MessageBase, session: AsyncSession = Depends(get_session)):
+
+    if payload.role != Role.USER:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User should be used only")
+
+    history = await list_all_conversations(session=session)
+    full_history = history + [
+        {"role":"user", "content":payload.content, "time_stamp": str(datetime.now(timezone.utc))}
+    ]
+
+    crew_input = {
+        "history": full_history,
+    }
+    crew_output = await run_in_threadpool(workflow_orchestrator, inputs=crew_input)
+
+    user_msg = Message(
+        role=Role.USER,
+        content=payload.content
+    )
+
+    asst_msg = Message(
+        role=Role.ASSISTANT,
+        content=crew_output
+    )
+
+    session.add(user_msg)
+    session.add(asst_msg)
+
+    await session.commit()
+    await session.refresh(user_msg)
+    await session.refresh(asst_msg)
+
+    return asst_msg
 
 
 @app.delete("/clear-message", status_code=status.HTTP_204_NO_CONTENT)
