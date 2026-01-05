771c05963734d705f4bcc6d7170ff55857127a5a
Nana Mein <alfeoephraimquintay@gmail.com>
2026-01-05 16:26:51 +0800

    Minimal config and will only put what i only need for now.

--- main.py
+++ main.py
@@ -1,32 +1,8 @@
-from typing import Optional
-
-from fastapi import FastAPI, status, HTTPException
-from pydantic import BaseModel
-from dotenv import load_dotenv
-import os
-from routers.message_router import router as message_router
+from fastapi import FastAPI
 from routers.auth_router import router as auth_router
 
-load_dotenv()
-
 app = FastAPI()
-app.include_router(message_router)
 app.include_router(auth_router)
-
-class HelloWorld(BaseModel):
-    message: Optional[str] = None
-    is_it_success: bool = True
-
-@app.post("/", response_model=HelloWorld, status_code=status.HTTP_200_OK)
-def hello_world(hello: HelloWorld):
-    if not hello.message:
-        hello.message = os.getenv("HELLO")
-
-    if not hello.is_it_success:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="No Hello World")
-    else:
-        return hello
-
 
 
 if __name__ == "__main__":
@@ -35,6 +11,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8080,
+        port=8000,
         reload=True
     )
