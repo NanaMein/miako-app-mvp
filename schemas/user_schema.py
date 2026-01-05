172b7b972216c1719beccfa80bcca346566d0b64
Nana Mein <alfeoephraimquintay@gmail.com>
2026-01-05 16:21:30 +0800

    Modify class UserCreate and added new class UserLogin. Not final but this is enough for now.

--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -6,7 +6,11 @@ class UserBase(SQLModel):
     email: str = Field(index=True, unique=True)
 
 class UserCreate(UserBase):
+    username: str
     password: str
 
 class UserRead(UserBase):
     uuid: uuid.UUID
+
+class UserLogin(UserBase):
+    password: str
