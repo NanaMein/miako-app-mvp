53aff5b63d8117230f8c786092912d6d7d8e673e
Nana Mein Patatas <alfeoephraimquintay@gmail.com>
2026-01-06 23:52:17 +0800

    Add JWT and Simple endpoint (#39)
    * Fixed by adding missing __init__.py
    
    * Added core directory. Security config such as jwt and password hashing will exist here.
    
    * Added auth router. User login and User Sign up endpoints are here with simple error handling. Initial testing works fine for now.
    
    * Modify class UserCreate and added new class UserLogin. Not final but this is enough for now.
    
    * Added Auth router in main.py
    
    * Minimal config and will only put what i only need for now.
    
    * Fixed by adding missing __init__.py
    
    * Added core directory. Security config such as jwt and password hashing will exist here.
    
    * Added auth router. User login and User Sign up endpoints are here with simple error handling. Initial testing works fine for now.
    
    * Modify class UserCreate and added new class UserLogin. Not final but this is enough for now.
    
    * Added Auth router in main.py
    
    * Minimal config and will only put what i only need for now.
    
    * Added logout endpoint
    
    * Added missing __init__ file
    
    * Modify names for naming convention
    
    * Added config file for jwt
    
    * Added jwt into security. Access token and refresh token generator added. Ive added verification but would add little more tweak feature. Would test first before adding more.
    
    * Added CORSMiddleware for middleware pipeline. This also the foundation or core part for me to use httponly cookie.
    
    * Added Setup of cookies. This will make my token be added in httponly cookie for protection. This applies to both access and refresh token
    
    * Made jwt easier by getting id in subject while it also refreshes token when access expires automatically without frontend doing anything.
    
    * When login, this add access and refresh token directly to httponly cookie, making sure that jwt is secured in web browser.
    
    * Made config.py well configured. Previously the Config class was deprecated so to prevent future bugs, i decided to use the latest ones. But ide seems it things that this is an error, but running the code works for me.
    
    * Modified if payload for refresh and access token carries tokens or the type of token they carry. If not it would raise an Error instead of returning none. We assume that user_id exist and if it doesn't it raises an error. Added Logout feature to remove cookies.
    
    * Added max age to make sure Users wont have worse UX when web browser restarts or closes. This make sures that refresh token stays in browser.
    
    * Import my security.py in auth_router. Now it can do hash password, set cookie during login, and delete cookie after logout.

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
-from typing import Optional
-
-from fastapi import FastAPI, status, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers.auth_router import router as auth_router
 from dotenv import load_dotenv
 import os
-from routers.message_router import router as message_router
 
 load_dotenv()
 
 app = FastAPI()
-app.include_router(message_router)
 
-class HelloWorld(BaseModel):
-    message: Optional[str] = None
-    is_it_success: bool = True
+origins=[os.getenv("ALLOW_ORIGIN")]
 
-@app.post("/", response_model=HelloWorld, status_code=status.HTTP_200_OK)
-def hello_world(hello: HelloWorld):
-    if not hello.message:
-        hello.message = os.getenv("HELLO")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-    if not hello.is_it_success:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="No Hello World")
-    else:
-        return hello
-
+app.include_router(auth_router)
 
 
 if __name__ == "__main__":
@@ -33,6 +27,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8080,
+        port=8000,
         reload=True
     )
