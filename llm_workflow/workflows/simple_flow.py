4635cf2f0e6ac4105302ed9a58781b0bcbfa20d4
Nana Mein <alfeoephraimquintay@gmail.com>
2026-01-10 18:04:59 +0800

    refactor: Simplified this file, also this would be used for testing llm workflow/multi-llm step workflow.

--- llm_workflow/workflows/simple_flow.py
+++ llm_workflow/workflows/simple_flow.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from dotenv import load_dotenv
+from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import delete
 from models.message_model import Message
@@ -6,29 +9,13 @@ from schemas.message_schema import MessageBaseSchema, Role
 from databases.database import get_session
 from llm_workflow.workflows.flow_chatbot import FlowMainWorkflow
 from fastapi import FastAPI, HTTPException, status, Depends
+from llm_workflow.workflows.main_workflow import MainFlowStates, MainWorkflow
 
-load_dotenv()
-flow_start = FlowMainWorkflow()
+flow_ = MainWorkflow()
 app = FastAPI()
 
 
 
-@app.post("/flow-send", response_model=MessageBaseSchema)
-async def flow_send(payload: MessageBaseSchema, session: AsyncSession = Depends(get_session)):
-    try:
-        payload_input = {
-            "input_message":payload.content,
-            "input_user_id":payload.role,
-            "async_session": session
-        }
-        output_flow = await flow_start.kickoff_async(inputs=payload_input)
-        assistant_message = {"role":Role.ASSISTANT.value,"content":output_flow}
-        return assistant_message
-    except Exception as ex:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail=f"Error error error: {ex}"
-        )
 
 
 @app.delete("/clear-message", status_code=status.HTTP_204_NO_CONTENT)
@@ -37,13 +24,44 @@ async def clear_message(session: AsyncSession = Depends(get_session)):
     await session.commit()
 
 
+async def inputs_test(**kwargs):
+    return await flow_.kickoff_async(inputs={**kwargs})
+
+class FlowService:
+    def __init__(self, **kwargs):
+        self.service=flow_.kickoff_async(inputs={**kwargs})
+
+class PayloadValidation(BaseModel):
+    message: str
+
+
+@app.post("/flow-main-send", response_model=PayloadValidation)
+async def flow_send(payload: PayloadValidation, session: AsyncSession = Depends(get_session)):
+    try:
+
+        flow_serve = FlowService(
+            input_user_id="test",
+            input_message=payload.message,
+            async_session=session
+        )
+        service = await flow_serve.service
+        return PayloadValidation(message=service)
+    except Exception as ex:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Error error error: {ex}"
+        )
+
 if __name__ == "__main__":
+
 
     import uvicorn
     print("RUNNING UVICORN")
     uvicorn.run(
-        "flow_chatbot:app",
+        "simple_flow:app",
         host="0.0.0.0",
-        port=8888,
+        port=8000,
         reload=True
     )
+
+
