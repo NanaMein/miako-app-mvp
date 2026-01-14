ca6b1b70c23ed7d4a40686dd3aa6a81f0e1fb154
Nana Mein <alfeoephraimquintay@gmail.com>
2026-01-14 21:43:23 +0800

    refactor: Change os.getenv() to settings from core when calling getting ALLOW_ORIGINS

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers.auth_router import router as auth_router
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
+from core.config import settings
+# from dotenv import load_dotenv
+# import os
+# load_dotenv()
 
 app = FastAPI()
 
-origins=[os.getenv("ALLOW_ORIGIN")]
+# origins=[os.getenv("ALLOW_ORIGIN")]
+ORIGINS=settings.ALLOW_ORIGINS
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
