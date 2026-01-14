a0c4ed2d5f4de25a73b00bee6ec28c5769abbec8
Nana Mein <alfeoephraimquintay@gmail.com>
2026-01-14 21:45:04 +0800

    refactor: Change MDOMAIN to DOMAIN for better naming sense, i was about to do Mock Domain or miako domain but its too long so i shortened it. Now i set to domain for better naming convention.

--- core/config.py
+++ core/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 5
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
-    MDOMAIN: str = Field(...)
+    DOMAIN: str = Field(...)
     DATABASE_URL: SecretStr
 
     model_config = SettingsConfigDict(
