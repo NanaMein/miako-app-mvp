b64c2a53837fd65105fd3da7c5646949619106e3
Nana Mein <alfeoephraimquintay@gmail.com>
2026-01-14 13:09:46 +0800

    refactor: Made small adjustment to locking.py. Because there might be a day to be reused to different scenario like not just vector but also in-memory dict cache. Any ways ive added cache to default cache so anyone can just use them to create lock with caching mechanism.

--- llm_workflow/config_files/locking.py
+++ llm_workflow/config_files/locking.py
@@ -3,37 +3,45 @@ from typing import Optional, Union, Any
 import asyncio
 
 
-MANAGER_LIST_CACHE = LRUCache(maxsize=1000)
-MANAGER_LOCK = asyncio.Lock()
+DEFAULT_LIST_CACHE = LRUCache(maxsize=1000)
+DEFAULT_LOCK = asyncio.Lock()
 
 class LockManager:
-    def __init__(self, user_id: Union[str, Any] = None):
+    def __init__(
+            self, user_id: Union[str, Any],
+            lru_cache: Optional[LRUCache] = DEFAULT_LIST_CACHE,
+            asyncio_lock: Optional[asyncio.Lock] = DEFAULT_LOCK,
+
+    ):
+
         self._user_id = user_id
         self.get_user_lock = self.get_lock()
+        self.manager_list_cache = lru_cache
+        self.manager_lock = asyncio_lock
 
     async def get_lock(self) -> asyncio.Lock:
-        user_id = self._user_id
+        user_id =str(self._user_id)
 
-        if user_id in MANAGER_LIST_CACHE:
-            return MANAGER_LIST_CACHE[user_id]
+        if user_id in self.manager_list_cache:
+            return self.manager_list_cache[user_id]
 
-        async with MANAGER_LOCK:
+        async with self.manager_lock:
 
-            if user_id in MANAGER_LIST_CACHE:
-                return MANAGER_LIST_CACHE[user_id]
+            if user_id in self.manager_list_cache:
+                return self.manager_list_cache[user_id]
 
             new_user_lock = asyncio.Lock()
-            MANAGER_LIST_CACHE[user_id] = new_user_lock
+            self.manager_list_cache[user_id] = new_user_lock
             return new_user_lock
 
     async def get_lock_with_id(self, user_id: Optional[str]):
-        if user_id in MANAGER_LIST_CACHE:
-            return MANAGER_LIST_CACHE[user_id]
+        if user_id in self.manager_list_cache:
+            return self.manager_list_cache[user_id]
 
-        async with MANAGER_LOCK:
-            if user_id in MANAGER_LIST_CACHE:
-                return MANAGER_LIST_CACHE[user_id]
+        async with self.manager_lock:
+            if user_id in self.manager_list_cache:
+                return self.manager_list_cache[user_id]
 
             new_user_lock = asyncio.Lock()
-            MANAGER_LIST_CACHE[user_id] = new_user_lock
+            self.manager_list_cache[user_id] = new_user_lock
             return new_user_lock
