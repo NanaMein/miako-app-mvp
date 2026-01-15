a1377e61311155fdb5bcc0e4e565670c63a3f971
Nana Mein <alfeoephraimquintay@gmail.com>
2026-01-15 22:36:20 +0800

    feat: Added new endpoint only for the main workflow logic to be used and tested.

--- llm_workflow/workflows/simple_flow.py
+++ llm_workflow/workflows/simple_flow.py
@@ -9,9 +9,9 @@ from databases.database import get_session
 from llm_workflow.workflows.flow_chatbot import FlowMainWorkflow
 from fastapi import FastAPI, HTTPException, status, Depends
 from llm_workflow.chat_completions.groq_llm import ChatCompletionsClass
-from llm_workflow.workflows.main_workflow import MainFlowStates, MainWorkflow
+from llm_workflow.workflows.main_workflow import MainFlowStates, LLMWorkflow, flow_kickoff
 
-flow_ = MainWorkflow()
+flow_ = LLMWorkflow()
 app = FastAPI()
 
 
@@ -48,7 +48,7 @@ SAMPLE_CONVERSATION= [
 
     make_message("user","Wala po akong external monitor. May iba pong paraan? Baka po ba ito hardware issue?")
 ]
-print(SAMPLE_CONVERSATION)
+# print(SAMPLE_CONVERSATION)
 
 chat_comp = ChatCompletionsClass()
 
@@ -88,6 +88,21 @@ async def flow_send(payload: PayloadValidation, session: AsyncSession = Depends(
         )
         service = await flow_serve.service
         return PayloadValidation(message=service)
+    except Exception as ex:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Error error error: {ex}"
+        )
+
+@app.post("/v1/flow-main-send", response_model=PayloadValidation)
+async def flow_send(payload: PayloadValidation, session: AsyncSession = Depends(get_session)):
+    try:
+        flow_object = await flow_kickoff(
+            input_user_id="test",
+            input_message=payload.message,
+            async_session=session
+        )
+        return PayloadValidation(message=flow_object)
     except Exception as ex:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
