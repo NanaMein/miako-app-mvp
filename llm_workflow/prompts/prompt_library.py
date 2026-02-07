698128ec077c3c581ab0603ce9691c10966e1f80
Nana Mein <alfeoephraimquintay@gmail.com>
2026-02-07 22:32:24 +0800

    Refactor: Integrating Solid code implementation while removing the explicit path finding, with just instance in the script regardless where it is in nested folders, it can now be access easily.

--- llm_workflow/prompts/prompt_library.py
+++ llm_workflow/prompts/prompt_library.py
@@ -1,30 +1,49 @@
 import yaml
 import json
 from typing import Dict, Union, Any
+from abc import ABC, abstractmethod
+from pathlib import Path
 from llama_index.core.prompts import PromptTemplate
+try:
+    from importlib import resources
+except ImportError:
+    import importlib_resources as resources
 
 
-class PromptLibrary:
-    def __init__(self, file_path: str = "prompts.yaml"):
-        self.filepath = file_path
-        self.prompt = self._load_yaml()
+class LoaderABC(ABC):
+    def __init__(self, file_path: Union[str, None] = None):
+        self.file_path = file_path
 
-    def _load_yaml(self) -> Dict[str, Any]:
+    @abstractmethod
+    def _load_resources(self) -> Dict[str, Any]:
+        pass
+
+    @abstractmethod
+    def get_prompt(self, key_path: str) -> str:
+        pass
+
+
+class LibraryLoader(LoaderABC):
+    def __init__(self, file_path: Union[str, None] = None):
+        super().__init__(file_path)
+        self.base_dir = Path(__file__).parent
+        self.data = self._load_resources()
+
+    def _load_resources(self) -> Dict[str, Any]:
         try:
-            with open(self.filepath, "r", encoding="utf-8") as file:
-                return yaml.safe_load(file)
+            full_path = self.base_dir/self.file_path
+            with open(full_path, "r", encoding="utf-8") as file:
+                return yaml.safe_load(file) or {}
 
-        except FileNotFoundError:
+        except (FileNotFoundError, yaml.YAMLError):
             return {}
 
 
     def get_prompt(self, key_path: str) -> str:
-        keys = key_path.split(".")
-        data = self.prompt
-
+        data = self.data
         try:
-            for key in keys:
-                data = data[key]
+            for keys in key_path.split("."):
+                data = data[keys]
 
             if isinstance(data, (dict,list)):
                 return json.dumps(data, indent=2, ensure_ascii=False)
@@ -39,3 +58,15 @@ class PromptLibrary:
         template_obj = PromptTemplate(template=template)
         final_template = template_obj.format(**kwargs)
         return final_template
+
+
+class BasePrompt:
+    def __init__(self, yaml_file_name: Union[str, None] = None):
+        self.loader = LibraryLoader(file_path=yaml_file_name)
+
+    def get_prompt(self, key_path: str) -> str:
+        return self.loader.get_prompt(key_path=key_path)
+
+class PromptLibrary(BasePrompt):
+    def __init__(self):
+        super().__init__("prompts.yaml")
