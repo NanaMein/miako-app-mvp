18050974b38356378cb238127400b838b436b05b
Nana Mein <alfeoephraimquintay@gmail.com>
2026-02-14 11:29:47 +0800

    Feature: Added listener and router inside flow, which can now identify, translate and update memory with ease.

--- llm_workflow/workflows/steps/language_step.py
+++ llm_workflow/workflows/steps/language_step.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, ConfigDict
 from llm_workflow.memory.short_term_memory.message_cache import MessageStorage
 from llm_workflow.prompts.prompt_library import PromptLibrary, BasePrompt
 from llm_workflow.llm.groq_llm import ChatCompletionsClass as LLMGroq
+import asyncio
 
 
 library = PromptLibrary()
@@ -42,37 +43,38 @@ class _LanguageRouter(Flow[LanguageState]):
         _user_id = f"translated_x_{self.state.user_id}"
         return MessageStorage(user_id=_user_id)
 
-    @start
+    @start()
     async def english_identifier(self) -> str:
+        print("Running: english_identifier")
         return await self._english_identifier(self.state.original_message)
 
 
     @router(english_identifier)
-    def english_router(self, identified_language: str):
+    def english_router(self, identified_language):
+        print("Running: english_router")
         return self._english_router(identified_language)
 
-    @listen("english_router_passed")
+    @listen("ENGLISH_PASSED")
     def english_router_passed(self):
+        print("Running: english_router_passed")
         return self.state.original_message
 
-    @listen("english_router_failed")
+    @listen("ENGLISH_FAILED")
     async def english_router_failed(self):
+        print("Running: english_router_failed")
         return await self._translate_to_english(self.state.original_message)
 
     @listen(or_(english_router_passed, english_router_failed))
     async def memory_update(self, message):
+        print(f"Running: memory_update -> {message}")
         await self.original_memory.add_human_message(self.state.original_message)
         await self.translated_memory.add_human_message(message)
         return message
 
-    @listen(memory_update)
-    def final_answer(self, message):
-        return message
 
 
 
-
-    async def _english_identifier(self, input_message: str):
+    async def _english_identifier(self, input_message):
         system_message = LIB.get_prompt("language_classifier.current")
         self.llm.add_system(system_message)
         self.llm.add_user(input_message)
@@ -86,9 +88,9 @@ class _LanguageRouter(Flow[LanguageState]):
 
         if upper_response in options:
             if upper_response == "YES":
-                return "english_router_passed"
+                return "ENGLISH_PASSED"
             elif upper_response == "NO":
-                return "english_router_failed"
+                return "ENGLISH_FAILED"
             elif upper_response == "UNKNOWN":
                 return "error_db"
             else:
@@ -101,6 +103,7 @@ class _LanguageRouter(Flow[LanguageState]):
         self.llm.add_system(system_message)
         self.llm.add_user(input_message)
         response = await self.llm.groq_maverick()
+        print(response)
         return str(response)
 
 
@@ -108,4 +111,37 @@ class _LanguageRouter(Flow[LanguageState]):
 
     @listen("error_db")
     def error_db(self):
+        print("error_db")
         return None
+
+
+
+    @listen(memory_update)
+    def final_answer(self, message):
+        print("Running: final_answer")
+        return message
+
+
+class LanguageFlow:
+    def __init__(self, user_id: str, original_message: str):
+        self.original_message = original_message
+        self.user_id = user_id
+        self.flow = _LanguageRouter()
+
+    async def run(self):
+        return await self.flow.kickoff_async(
+            {
+                "user_id": self.user_id,
+                "original_message": self.original_message,
+            }
+        )
+
+
+
+_lang = LanguageFlow("user_123", "hello")
+
+async def get_lang():
+    result = await _lang.run()
+    print(result)
+    return result
+asyncio.run(get_lang())
