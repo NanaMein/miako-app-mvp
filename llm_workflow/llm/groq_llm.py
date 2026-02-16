d5b89f92ad6fe6b3eb849f51bb6bba9f9579f7a0
Nana Mein <alfeoephraimquintay@gmail.com>
2026-02-16 23:47:58 +0800

    Feature: Added GroqLLM class that uses the ChatBase as abc. Now this is another alternative to use groq effectively and with flexible, no need to change the function and just add and remove content to its parameter. I also added models class for the easier access and use model without needing to hardcode each and every model. Also compare to my old code that uses _model, this is easier because content is seen and much easier to understand and use.

--- llm_workflow/llm/groq_llm.py
+++ llm_workflow/llm/groq_llm.py
@@ -2,6 +2,24 @@ from typing import Optional
 from groq import AsyncGroq
 from functools import lru_cache
 from llm_workflow.config_files.config import workflow_settings
+from abc import ABC, abstractmethod
+
+
+class GroqModelList:
+    compound_beta =  "compound-beta"
+    compound_beta_mini = "compound-beta-mini"
+    instant = "llama-3.1-8b-instant"
+    versatile =  "llama-3.3-70b-versatile"
+    scout = "meta-llama/llama-4-scout-17b-16e-instruct"
+    maverick = "meta-llama/llama-4-maverick-17b-128e-instruct"
+    llama_guard =  "meta-llama/llama-guard-4-12b"
+    kimi =  "moonshotai/kimi-k2-instruct-0905"
+    gpt_oss_20 = "openai/gpt-oss-20b"
+    gpt_oss_120 = "openai/gpt-oss-120b"
+    qwen = "qwen/qwen3-32b"
+
+MODEL = GroqModelList()
+
 
 @lru_cache()
 def get_groq_client():
@@ -12,46 +30,18 @@ class ChatBase(ABC):
     def __init__(self):
         self.cached_messages = []
 
-    @property
-    def client(self) -> AsyncGroq:
-        return get_groq_client()
-
+    @abstractmethod
     def add_system(self, content: str = ""):
-        self._add_msg("system", content)
-        return self
+        pass
 
+    @abstractmethod
     def add_user(self, content: str = ""):
-        self._add_msg("user",content)
-        return self
+        pass
 
+    @abstractmethod
     def add_assistant(self, content: str = ""):
-        self._add_msg("assistant", content)
-        return self
+        pass
 
-    def _add_msg(self, role: str, content: str = ""):
-        if content and content.strip():
-            self.cached_messages.append({"role": role, "content": content})
-
-
-
-    async def _pipeline(self, model: str, is_message_content: bool = True, **kwargs) -> Union[str, ChatCompletionMessage]:
-        kwargs.setdefault("temperature", 0)
-        kwargs.setdefault("max_completion_tokens", 8000)
-        kwargs.setdefault("top_p", 1)
-        kwargs.setdefault("stop", None)
-        kwargs.setdefault("stream", False)
-
-        completion = await self.client.chat.completions.create(
-            model=_model(model=model),
-            messages=self.cached_messages,
-            **kwargs
-        )
-        pre_content = completion.choices[0].message
-
-        if is_message_content:
-            return pre_content.content
-        else:
-            return pre_content
 
 
 
@@ -123,3 +113,61 @@ def _model(model: str) -> Optional[str]:
     }
 
     return choices.get(model)
+
+
+class GroqLLM(ChatBase):
+
+    def __init__(self):
+        super().__init__()
+
+    @property
+    def client(self) -> AsyncGroq:
+        return get_groq_client()
+
+    def add_system(self, content: str = ""):
+        self._add_msg("system", content)
+        return self
+
+    def add_user(self, content: str = ""):
+        self._add_msg("user",content)
+        return self
+
+    def add_assistant(self, content: str = ""):
+        self._add_msg("assistant", content)
+        return self
+
+    def _add_msg(self, role: str, content: str = ""):
+        if content and content.strip():
+            self.cached_messages.append({"role": role, "content": content})
+
+
+    @staticmethod
+    def _setting_defaults(**kwargs):
+        kwargs.setdefault("temperature", 0)
+        kwargs.setdefault("max_completion_tokens", 8000)
+        kwargs.setdefault("top_p", 1)
+        kwargs.setdefault("stop", None)
+        kwargs.setdefault("stream", False)
+        return kwargs
+
+
+    async def _pipeline(self, model: str, return_as_object: bool, **kwargs):
+        _kwargs = self._setting_defaults(**kwargs)
+
+        completion = await self.client.chat.completions.create(
+            model=model,
+            messages=self.cached_messages,
+            **_kwargs
+        )
+        pre_content = completion.choices[0].message
+
+        if return_as_object:
+            return pre_content
+        else:
+            return pre_content.content
+
+    async def groq_chat(self, model: str, **kwargs) -> str:
+        return await self._pipeline(model=model, return_as_object=False, **kwargs)
+
+    async def groq_message_object(self, model: str, return_as_object: bool = True, **kwargs) -> Union[str, ChatCompletionMessage]:
+        return await self._pipeline(model=model, return_as_object=return_as_object, **kwargs)
