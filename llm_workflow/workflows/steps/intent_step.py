f4f95d3a3256ad30ad7567d13e7606eb1fb93fc8
Nana Mein <alfeoephraimquintay@gmail.com>
2026-02-17 21:55:02 +0800

    Feature: Added necessary functions that makes the intent classifier class as a whole. Added type hints because it is base model and finally a minimal testing, now that it works, ill start refactoring the prompts starting here and some needed modification and refactor parts of the file. And remove redundant functions that we wont need since we have the new class

--- llm_workflow/workflows/steps/intent_step.py
+++ llm_workflow/workflows/steps/intent_step.py
@@ -3,6 +3,9 @@ from crewai.flow.flow import Flow, start, listen, and_, or_
 from pydantic import BaseModel, ConfigDict
 from llm_workflow.llm.groq_llm import GroqLLM, MODEL
 from llm_workflow.prompts.prompt_library import BasePrompt
+from groq.types.chat import ChatCompletionMessage
+import asyncio
+
 
 class IntentLibrary(BasePrompt):
     def __init__(self):
@@ -22,37 +25,12 @@ class IntentResponse(BaseModel):
     parameters: dict
 
 
-async def intent_classifier(input_message: str):
-    system_prompt = RESOURCES.intent_library.get_prompt("intent_classifier.gemini_series.version_1")
-    RESOURCES.llm.add_system(system_prompt)
-    RESOURCES.llm.add_user(input_message)
-    chat_response = await RESOURCES.llm.groq_chat(model=MODEL.maverick)
-    return parsing_intent_data(input_intent=chat_response)
-
-def parsing_intent_data(input_intent: str):
-    intent_json_data = IntentResponse.model_validate_json(input_intent)
-    return intent_json_data
-
-
-def intent_router(_intent_data: IntentResponse):
-    intent_action = _intent_data.action
-    if intent_action == "web_search":
-        return "WEB_SEARCH"
-    elif intent_action == "rag_query":
-        return "RAG_QUERY"
-    elif intent_action == "direct_reply":
-        return "DIRECT_REPLY"
-    elif intent_action == "system_op":
-        return "SYSTEM_OP"
-    else:
-        return "UNKNOWN"
-
 class IntentState(BaseModel):
     user_id: Union[str, Any] = ""
     input_message: str = ""
     unparsed_intent_data: str = ""
     intent_data: Optional[IntentResponse] = None
-    system_prompt = RESOURCES.intent_library.get_prompt("intent_classifier.current")
+    system_prompt: str = RESOURCES.intent_library.get_prompt("intent_classifier.current")
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
@@ -66,4 +44,48 @@ class IntentClassifier(Flow[IntentState]):
     async def intent_classifier(self):
         self.llm.add_system(self.state.system_prompt)
         self.llm.add_user(self.state.input_message)
-        return await self.llm.groq_chat(model=MODEL.maverick)
+        response = await self.llm.groq_message_object(model=MODEL.maverick)
+        return response
+
+
+    @listen(intent_classifier)
+    def data_parsing(self, initial_intent_data: ChatCompletionMessage | str):
+        if isinstance(initial_intent_data, ChatCompletionMessage):
+            intent_string = initial_intent_data.content
+        else:
+            intent_string = initial_intent_data
+
+        intent_json = self._intent_validate_json(intent_string)
+        return intent_json
+
+    @listen(data_parsing)
+    def intent_action(self, intent_json: IntentResponse):
+        action = self._intent_action_router(intent_data=intent_json)
+        return action
+
+
+    @staticmethod
+    def _intent_action_router(intent_data: IntentResponse):
+        intent_action = intent_data.action
+        if intent_action == "web_search":
+            return "WEB_SEARCH"
+        elif intent_action == "rag_query":
+            return "RAG_QUERY"
+        elif intent_action == "direct_reply":
+            return "DIRECT_REPLY"
+        elif intent_action == "system_op":
+            return "SYSTEM_OP"
+        else:
+            return "UNKNOWN"
+
+
+    @staticmethod
+    def _intent_validate_json(data: str):
+        _intent_json = IntentResponse.model_validate_json(data)
+        return _intent_json
+
+
+x = IntentClassifier()
+_xx = x.kickoff_async({"input_message":"hello world"})
+xxx = asyncio.run(_xx)
+print(xxx)
