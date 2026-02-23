d7659543e041e6c30b6d10b75284659d9ac8eaf3
Nana Mein <alfeoephraimquintay@gmail.com>
2026-02-23 17:40:51 +0800

    Refactor: Just integrated the Intent Classifier Flow in flows.py while removing the old intent classifier. Removed ChatCompletionClass and replace it with the new GroqLLM. Updated the MessageStorage so it aligned with the langauge and intent internal memory storage logic. Remove Router because we wont need them, I'll make new router that isn't a one way path but a multi path that my llm can decide what actions to be executed (like agents using tools)

--- llm_workflow/workflows/flows.py
+++ llm_workflow/workflows/flows.py
@@ -5,12 +5,13 @@ from pydantic import BaseModel, ConfigDict, Field
 from datetime import datetime, timezone
 from sqlalchemy.ext.asyncio import AsyncSession
 from llm_workflow.memory.short_term_memory.message_cache import MessageStorage
-from llm_workflow.llm.groq_llm import ChatCompletionsClass
+from llm_workflow.llm.groq_llm import GroqLLM, MODEL
 from llm_workflow.prompts.prompt_library import PromptLibrary
 from fastapi import status, HTTPException
 from typing import Literal
 from dataclasses import dataclass, asdict
 from llm_workflow.workflows.steps.language_step import LanguageFlow
+from llm_workflow.workflows.steps.intent_step import IntentFlow
 
 
 class AppResources:
@@ -42,7 +43,6 @@ class EngineStates(BaseModel):
     input_message: str = Field(default="", description="User input message to llm workflow")
     input_user_id: str = Field(default="")
     intent_data: Optional[IntentResponse] = Field(default=None, description="Current intent data after translation")
-    unparsed_intent_data: str = ""
     async_session: Optional[AsyncSession] = None
     model_config = ConfigDict(arbitrary_types_allowed=True)
     time_stamp: str = Field(default_factory=lambda: date_time_now())
@@ -52,10 +52,10 @@ class EngineStates(BaseModel):
 
 class _AdaptiveChatbotEngine(Flow[EngineStates]):
     def __init__(self, **kwargs: Any):
+        self._original_memory: MessageStorage | None = None
+        self._translated_memory: MessageStorage | None = None
         super().__init__(**kwargs)
-        self.intent_classifier_llm = ChatCompletionsClass()
-        self.original_memory = MessageStorage(f"Original_chat_{self.state.input_user_id}")
-        self.translated_memory = MessageStorage(f"Translated_chat_{self.state.input_user_id}")
+        self.chatbot = GroqLLM()
 
 
 
@@ -73,84 +73,62 @@ class _AdaptiveChatbotEngine(Flow[EngineStates]):
         return await language_flow.run()
 
     @listen(language_layer)
-    async def intent_classifier(self, answer):
-        system_prompt = RESOURCES.library.get_prompt("intent_classifier.gemini_series.version_1")
-        self.intent_classifier_llm.add_system(system_prompt)
-        self.intent_classifier_llm.add_user(answer)
-        chat_response = await self.intent_classifier_llm.groq_maverick()
-        return self.parsing_intent_data(input_intent=chat_response)
+    async def intent_classifier(self, translation_response):
+        intent_flow = IntentFlow(
+            user_id=self.state.input_user_id,
+            original_user_input=self.state.input_message,
+            translated_user_input=translation_response
+        )
+        return await intent_flow.run()
 
-    def parsing_intent_data(self, input_intent: str):
-        self.state.unparsed_intent_data = input_intent
-        intent_json_data = IntentResponse.model_validate_json(input_intent)
-        self.state.intent_data = intent_json_data
-        return intent_json_data
-
-
-    @router(intent_classifier)
-    def intent_router(self, _intent_data: IntentResponse):
-        intent_action = _intent_data.action
-        if intent_action == "web_search":
-            return "WEB_SEARCH"
-        elif intent_action == "rag_query":
-            return "RAG_QUERY"
-        elif intent_action == "direct_reply":
-            return "DIRECT_REPLY"
-        elif intent_action == "system_op":
-            return "SYSTEM_OP"
-        else:
-            return "UNKNOWN"
-
-    @listen("WEB_SEARCH")
-    def web_search_route(self):
-        return self.state.intent_data
-
-    @listen("DIRECT_REPLY")
-    def direct_reply_route(self):
-        return self.state.intent_data
-
-    @listen("RAG_QUERY")
-    def rag_query_route(self):
-        return self.state.intent_data
-
-    @listen("SYSTEM_OP")
-    def system_op_route(self):
-        return self.state.intent_data
-
-    @listen(or_(web_search_route, direct_reply_route, rag_query_route, system_op_route))
-    async def memory_pipeline(self, intent_data: IntentResponse):
+    @listen(intent_classifier)
+    async def memory_pipeline(self, intent_data):
         system_prompt = """
-            ### System(Priming): You are a multilingual virtual assistant. You will answer the User and respond to the 
-            best of your abilities. User might ask weird request or direct questions, You will still answer and regardless
-            of the request is that you need to assist the user well. And then if it does weird request, at the end of your
-            respond, explain to the user what went wrong with how user ask you. Your name would be Miako, a sweet and supportive
-            assistant. 
-            
-            ### Instructions:
-            Translated and transformed chat conversation are considered context and follow the language used as the Original 
-            and continue to do so. If original is English, reply in english, if User use tagalog, reply in tagalog, if User
-            switch to other language, switch to other language too. You are a general purpose and conversational chatbot, being
-            adaptive to situations as you assist the User. 
+        ### System(Priming): You are a multilingual virtual assistant. You will answer the User and respond to the 
+        best of your abilities. User might ask weird request or direct questions, You will still answer and regardless
+        of the request is that you need to assist the user well. And then if it does weird request, at the end of your
+        respond, explain to the user what went wrong with how user ask you. Your name would be Miako, a sweet and supportive
+        assistant. 
+        
+        ### Instructions:
+        Translated and transformed chat conversation are considered context and follow the language used as the Original 
+        and continue to do so. If original is English, reply in english, if User use tagalog, reply in tagalog, if User
+        switch to other language, switch to other language too. You are a general purpose and conversational chatbot, being
+        adaptive to situations as you assist the User. 
         """
         orig_messages = await self.original_memory.get_messages()
         translated_messages = await self.translated_memory.get_messages()
         mock_memory = f"""
         ### Time: {self.state.time_stamp}
-        ### User Intents: {self.state.unparsed_intent_data}
+        ### User Intents: {intent_data}
         ### Translated and transformed previous chat conversation list (can be considered as context): 
         {translated_messages}
         ### Original and preserved chat conversation list: {orig_messages}
         ### User query: {self.state.input_message}
         ### Assistant:
         """
-        chatbot = ChatCompletionsClass()
-        chatbot.add_system(system_prompt)
-        chatbot.add_user(mock_memory)
-        response = await chatbot.groq_maverick()
+        # chatbot = GroqLLM()
+        self.chatbot.add_system(system_prompt)
+        self.chatbot.add_user(mock_memory)
+        response = await self.chatbot.groq_chat(model=MODEL.maverick, temperature=0.1)
         await self.original_memory.add_ai_message(response)
         await self.translated_memory.add_ai_message(response)
         get_msgs = await self.original_memory.get_messages()
         return response, get_msgs
+
+    @property
+    def original_memory(self):
+        if self._original_memory is None:
+            _user_id = f"original_x_{self.state.input_user_id}"
+            self._original_memory = MessageStorage(user_id=_user_id)
+        return self._original_memory
+
+    @property
+    def translated_memory(self):
+        if self._translated_memory is None:
+            _user_id = f"translated_x_{self.state.input_user_id}"
+            self._translated_memory = MessageStorage(user_id=_user_id)
+        return self._translated_memory
 
 
 class AdaptiveChatbot:
@@ -171,7 +149,7 @@ class AdaptiveChatbot:
         inputs = InputData(input_user_id=self.user_id, input_message=self.input_message)
         return asdict(inputs)
 
-    async def run(self) -> Union[Any, str, None]:
+    async def run(self) -> Any | str | None:
         try:
             response, _ = await self.flow_engine.kickoff_async(inputs=self._input_data)
             if response is None:
