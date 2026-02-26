f9b6ed95949505c2ba8ef2f89c7a181a86bdc587
Nana Mein <alfeoephraimquintay@gmail.com>
2026-02-26 22:12:23 +0800

    Refactor: Change the concurrent simulator and async benchmarking functions for testing the main flow.

--- llm_workflow/workflows/concurrent_simulator_chatbot.py
+++ llm_workflow/workflows/concurrent_simulator_chatbot.py
@@ -1,187 +1,11 @@
-from typing import Optional, Any
-from crewai.flow import Flow, start, listen
-from llama_index.core import PromptTemplate
-from pydantic import BaseModel, ConfigDict
-from dotenv import load_dotenv
-import os
-from groq.types.chat import ChatCompletionUserMessageParam, ChatCompletionSystemMessageParam
-from groq import AsyncGroq
-from sqlmodel import select, desc
-from models.message_model import Message
-from schemas.message_schema import Role
-from sqlalchemy.ext.asyncio import AsyncSession
-from llm_workflow.memory.long_term_memory.memory_store import ConversationMemoryStore
 from llm_workflow.workflows.base import ChatbotExecutor
 from llm_workflow.workflows.flows import AdaptiveChatbot
 import time
 import asyncio
 
-load_dotenv()
 
 
 
-SYSTEM_PROMPT = """
-        ### System:
-        You are an emotional intelligent virtual assistant.
-
-        ## Characteristic and traits:
-        You are in a form of young adult and a girl. You're name is Mirai Aiko
-
-        ### Instructions:
-        You need to understand the context and understand the overall context. But
-        mainly focus on the present context and use the older conversation as
-        supporting context and knowledge. <context_1> are raw data from vector store. It has
-        score and since it is hybrid, it uses sparse and dense embedding for better accuracy.
-        <context_2> will show the latest 5 conversations. The higher the node score, the higher the relevance, but
-        you need to still check for when it really is what is talking about. This ensures that both context_1 and
-        context_2 help you understand long-term and short-term memory in chat conversation. """
-prompt_template_format = """
-        ### Contexts:
-        <context_1>{context_1}</context_1>
-        <context_2>{context_2}</context_2>
-
-        ### Expected output:
-        With all the context present, you need to focus or answer the latest message of user in context_2.
-        All of the context other than the latest message are supporting knowledge for contextual
-        understanding. You are a conversation virtual assistant, you will mostly reply to user in
-        conversational length. But be verbose only when you think that you need to or you are tasked
-        to be more explicit. Remember Context are just Context, no need to over explain about previous context. Being
-        aware of conversation is enough. No need to explain from this and that conversation or explicit explain
-        about previous conversation. Be natural and no need to express. As long as you are aware but no need to be
-        explicit
-        """
-
-PROMPT_TEMPLATE = PromptTemplate(template=prompt_template_format)
-
-
-
-class FlowMainStates(BaseModel):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-    input_message: str = ""
-    input_user_id: Optional[Any] = ""
-    async_session: Optional[AsyncSession] = None
-    context_1: str = ""
-    context_2: str = ""
-    output: str = ""
-
-
-
-class FlowMainWorkflow(Flow[FlowMainStates]):
-    def __init__(self, **kwargs: Any):
-        self._memory_store = None
-        self._async_groq = None
-        super().__init__(**kwargs)
-
-
-    @property
-    def memory_store(self):
-        if self._memory_store is None:
-            self._memory_store = ConversationMemoryStore()
-        return self._memory_store
-
-    @property
-    def async_groq(self):
-        if self._async_groq is None:
-            self._async_groq = AsyncGroq(api_key=os.getenv("GROQ_API_KEY"))
-        return self._async_groq
-
-    @start()
-    def start_with_safety_content(self):
-        pass
-
-
-    @listen(start_with_safety_content)
-    async def get_vector_store_memory(self):
-        user_id: str = self.state.input_user_id
-        context = await self.memory_store.get_memory(
-            user_id=user_id,
-            message=self.state.input_message
-        )
-        self.state.context_1 = context
-
-    @listen(get_vector_store_memory)
-    async def get_session_database(self):
-        limit: int = 10
-        stmt = (
-            select(Message)
-            .order_by(desc(Message.date_timestamp))
-            .limit(limit)
-        )
-        result = await self.state.async_session.execute(stmt)
-        messages = result.scalars().all()
-        reversed_message = messages[::-1]
-        history_context = ""
-
-        for msg in reversed_message:
-            if msg.role == Role.USER:
-                role_label = Role.USER.value
-
-            elif msg.role == Role.ASSISTANT:
-                role_label = Role.ASSISTANT.value
-
-            else:
-                role_label = "unidentified entity"
-            history_context += f"{role_label}: {msg.content}\n\n--/--/--/--/--\n\n"
-
-        self.state.context_2 = history_context
-
-    @listen(get_session_database)
-    def context_prompt(self):
-        message = PROMPT_TEMPLATE.format(
-            context_1=self.state.context_1,
-            context_2=self.state.context_2
-        )
-        return message
-
-    @listen(context_prompt)
-    async def groq_chat_bot(self, content: str):
-        system_: ChatCompletionSystemMessageParam={"role":"system","content":SYSTEM_PROMPT}
-        user_: ChatCompletionUserMessageParam = {"role":"user","content":content}
-
-        completion = await self.async_groq.chat.completions.create(
-            model="meta-llama/llama-4-maverick-17b-128e-instruct",
-            messages=[system_,user_],
-            temperature=1,
-            max_completion_tokens=8192,
-            top_p=1,
-            stream=False,
-            stop=None
-        )
-
-        content = completion.choices[0].message
-        self.state.output = content.content
-        return content.content
-
-    @listen(groq_chat_bot)
-    async def add_message_to_database(self, content:str):
-
-        user_msg = Message(
-            role=Role.USER.value,
-            content=self.state.input_message
-        )
-
-        asst_msg = Message(
-            role=Role.ASSISTANT.value,
-            content=content
-        )
-
-        self.state.async_session.add(user_msg)
-        self.state.async_session.add(asst_msg)
-
-        await self.state.async_session.commit()
-        await self.state.async_session.refresh(user_msg)
-        await self.state.async_session.refresh(asst_msg)
-
-        return content
-
-    @listen(add_message_to_database)
-    async def add_message_to_vector(self, assistant_message:str):
-        await self.memory_store.add_memory(
-            user_message=self.state.input_message,
-            assistant_message=assistant_message,
-            user_id=self.state.input_user_id
-        )
-        return assistant_message
 
 SAMPLE_TAGALOG = [
     "Kamusta? May problema po ako sa aking laptop. Hindi siya magsisimula.",
@@ -218,7 +42,7 @@ SAMPLE_BURMESE = [
 ]
 
 class SampleStates:
-    def __init__(self, sample: Optional[list] = None, state: int = 0):
+    def __init__(self, sample: list | None = None, state: int = 0):
         self.state = state
         self.lock = asyncio.Lock()
         self.sample = sample
@@ -255,76 +79,94 @@ class SampleStates:
         return _choice
 
 
-async def run_concurrent():
-    tasks = []
-    s = SampleStates()
-    for it in range (13):
-        message = await s.get_choice_async()
-        print(f"Message {str(s.state)}: {message}")
-        chatbot = AdaptiveChatbot(user_id=f"test_user_{s.state}", input_message=message)
-        flow = ChatbotExecutor(chatbot)
-        task = flow.execute()
-        tasks.append(task)
 
-    results = await asyncio.gather(*tasks, return_exceptions=True)
+async def execute_with_timer(user_id: str, message: str):
+    t0 = time.perf_counter()
 
-    for il, result in enumerate(results):
-        print(f"Result {il} (User test_user_{il}):\n{result}\n")
-        print("-" * 50)
+    chatbot = AdaptiveChatbot(user_id=user_id, input_message=message)
+    flow = ChatbotExecutor(chatbot)
+
+    t1 = time.perf_counter()
+
+    try:
+        result = await flow.execute()
+        success = True
+    except Exception as e:
+        result = str(e)
+        success = False
+
+    t2 = time.perf_counter()
+
+    return {
+        "user_id": user_id,
+        "success": success,
+        "total_time": t2 - t0,
+        "flow_time": t2 - t1,
+        "init_time": t1 - t0,
+        "result_length": len(str(result))
+    }
+
 
 async def run_concurrent_all_language():
     tasks = []
-    state_num = 0
-    range_num = 3
+    state_num = 5
+    range_num = 10
+
     tagalog = SampleStates(sample=SAMPLE_TAGALOG, state=state_num)
-    for tl in range (range_num):
-        tagalog_message = await tagalog.get_sample()
-        print(f"Message Tagalog: {str(tagalog.state)}: {tagalog_message}")
-        chatbot = AdaptiveChatbot(user_id=f"test_user_{tagalog.state}", input_message=tagalog_message)
-        flow = ChatbotExecutor(chatbot)
-        task = flow.execute()
-        tasks.append(task)
+    for _ in range(range_num):
+        msg = await tagalog.get_sample()
+        tasks.append(
+            execute_with_timer(
+                user_id=f"tagalog_{tagalog.state}",
+                message=msg
+            )
+        )
 
     lao = SampleStates(sample=SAMPLE_LAO, state=state_num)
-    for la in range(range_num):
-        lao_message = await lao.get_sample()
-        print(f"Message Lao: {str(lao.state)}: {lao_message}")
-        chatbot = AdaptiveChatbot(user_id=f"test_user_{lao.state}", input_message=lao_message)
-        flow = ChatbotExecutor(chatbot)
-        task = flow.execute()
-        tasks.append(task)
+    for _ in range(range_num):
+        msg = await lao.get_sample()
+        tasks.append(
+            execute_with_timer(
+                user_id=f"lao_{lao.state}",
+                message=msg
+            )
+        )
 
     burmese = SampleStates(sample=SAMPLE_BURMESE, state=state_num)
-    for bu in range(range_num):
-        burmese_message = await burmese.get_sample()
-        print(f"Message Burmese: {str(burmese.state)}: {burmese_message}")
-        chatbot = AdaptiveChatbot(user_id=f"test_user_{burmese.state}", input_message=burmese_message)
-        flow = ChatbotExecutor(chatbot)
-        task = flow.execute()
-        tasks.append(task)
+    for _ in range(range_num):
+        msg = await burmese.get_sample()
+        tasks.append(
+            execute_with_timer(
+                user_id=f"burmese_{burmese.state}",
+                message=msg
+            )
+        )
 
-    results = await asyncio.gather(*tasks, return_exceptions=True)
+    task_results = await asyncio.gather(*tasks, return_exceptions=False)
 
-    for il, result in enumerate(results):
-        print(f"Result {il} (User test_user_{il}):\n{result}\n")
+    for r in task_results:
+        print(f"[{r['user_id']}]")
+        print(f"Time: {r['execution_time']:.4f}s")
+        print(f"Success: {r['success']}")
+        print(f"Result: {r['result']}")
         print("-" * 50)
 
-class Timer:
-    def __init__(self):
-        self.timer = time.perf_counter()
+    return task_results
 
-    def now(self):
-        end_timer = time.perf_counter()
-        elapsed_time = end_timer - self.timer
-        return elapsed_time
+def summarize(_results):
+    times = [r["execution_time"] for r in _results if r["success"]]
+
+    print("\n=== SUMMARY ===")
+    print(f"Total requests: {len(_results)}")
+    print(f"Successful: {len(times)}")
+    print(f"Failed: {len(_results) - len(times)}")
+    print(f"Avg time: {sum(times)/len(times):.4f}s")
+    print(f"Min time: {min(times):.4f}s")
+    print(f"Max time: {max(times):.4f}s")
 
 if __name__ == "__main__":
-    t = Timer()
-    all_lang = asyncio.run(run_concurrent_all_language())
-    print("Type of the output:", type(all_lang))
-    print("~-/*" * 50)
-    print(f"Execution time in seconds: {t.now()}")
-    print("~-/*" * 50)
+    results = asyncio.run(run_concurrent_all_language())
+    summarize(results)
 
 
 
