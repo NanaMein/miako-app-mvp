f7a6cb1089c6750f37e6bb8cb24aa11a0e144712
Nana Mein <alfeoephraimquintay@gmail.com>
2026-03-16 00:01:19 +0800

    Chore: Change it back to comment. So that it doesn't conflict with running docker.

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ app = FastAPI()
 app.include_router(llm_router)
 
 
-if __name__ == "__main__":
-    import uvicorn
-    print("RUNNING UVICORN")
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True
-    )
+# if __name__ == "__main__":
+#     import uvicorn
+#     print("RUNNING UVICORN")
+#     uvicorn.run(
+#         "main:app",
+#         host="0.0.0.0",
+#         port=8000,
+#         reload=False
+#     )
