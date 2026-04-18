4651b79cc7fd273285cab0fb0d28d22ac4a24030
Nana Mein <alfeoephraimquintay@gmail.com>
2026-04-19 00:16:09 +0800

    Refactor: removed entrypoint.sh and changed it to execute.py. This makes it better since we will only use python and uv as interpreter inside the container. There were issues were entrypoint.sh not working in production environment.

---
+++ execute.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import subprocess
+import sys
+import os
+
+
+def run(cmd: list[str]):
+    print(f"▶️ Running: {' '.join(cmd)}", flush=True)
+    result = subprocess.run(cmd, check=True)
+    return result
+
+
+if __name__ == "__main__":
+    # Run Alembic migrations
+    run([sys.executable, "-m", "alembic", "upgrade", "head"])
+
+    # Start Uvicorn
+    os.execvp(
+        sys.executable,
+        [
+            sys.executable,
+            "-m", "uvicorn",
+            "main:app",
+            "--host", "0.0.0.0",
+            "--port", "8000",
+        ]
+    )
