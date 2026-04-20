4bce2320b3f14f5a6aae62cd056e7a7fb1b59ae1
Nana Mein <alfeoephraimquintay@gmail.com>
2026-04-20 20:54:58 +0800

    Chore: Found out that in init, we need to explicitly add these models as that alembic and sqlmodel can work without any problems.

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,3 @@
-from models.message_model import Message
-from models.conversation_model import Conversation
-from models.user_model import User
+from src.models.message_model import Message
+from src.models.conversation_model import Conversation
+from src.models.user_model import User
