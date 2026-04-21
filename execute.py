0602eb7ca877a037f005a83be4267bab1be5e03d
Nana Mein <alfeoephraimquintay@gmail.com>
2026-04-21 15:26:59 +0800

    Chore: Update execute.py because now that i transfer main.py inside src, just so that it will work inside docker container

--- execute.py
+++ execute.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         [
             sys.executable,
             "-m", "uvicorn",
-            "main:app",
+            "src.main:app",
             "--host", "0.0.0.0",
             "--port", "8000",
         ]
